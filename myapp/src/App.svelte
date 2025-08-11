83628155dfc31a8f30e4f7161e5504bea0552824
Mathias <mathias.lenglet@oclock.school>
2025-08-11 11:18:45 +0200

    set html header structure

--- myapp/src/App.svelte
+++ myapp/src/App.svelte
@@ -1,8 +1,40 @@
 <script>
+    import Icon from "@iconify/svelte";
 
 </script>
+<header>
+    <img src="" alt="logo Main Event">
+    <nav>
+        <a href="">Home</a>
+        <a href="">Events</a>
+        <a href="">Athletes</a>
+        <a href="">Rankings</a>
+        <a href="">News</a>
+    </nav>
+    <div>
+        <button type="button">
+            <Icon icon="hugeicons:search-01" width="24" height="24" />
+            Search</button>
+        <button type="button">
+            <Icon icon="hugeicons:user" width="24" height="24" />
+            Profile</button>
+    </div>
+        
+</header>
 
-<h1>Main Event</h1>
 <style>
+
+    :root{
+        --primaryColor: #0F1119;
+    }
+
+    :global(body){
+        background-color: var(--primaryColor);
+    }
+
+    header{
+        display: flex;
+        justify-content: space-between;
+    }
 
 </style>
